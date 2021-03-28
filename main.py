e29a26e3cacd577ce032f6a7d0e84252dc21af5c
jennifer66666 <jennifer611766@gmail.com>
2021-03-28 13:21:29 +0800

    various epsilon_0

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from .fgsm  import *
 def main():
     labels = read_in_labels("labels.txt")
     result = []
-    result_fgsm = []
+    result_fgsm_list = []
     result_fgsm_random = []
+    epsilon_0_list = [0.1*i for i in range(1,11)]
     model = Model("param.txt")
     for img_name in range(1,155):
         path = "pgm/"+str(img_name)+".pgm"
         x = Image(path).flattened_vector
-        x_fgsmized = fgsm_an_image(x,labels[img_name - 1],0.1,model)
+        x_fgsmized_list = [fgsm_an_image(x,labels[img_name - 1],epsilon_0,model) for epsilon_0 in epsilon_0_list]
         x_fgsmized_random = fgsm_an_image(x,labels[img_name - 1],0.1,model,for_test=True)
         result.append(model(x)["max_result"])
-        result_fgsm.append(model(x_fgsmized)["max_result"])
+        result_fgsm_list.append([model(x_fgsmized)["max_result"] for x_fgsmized in x_fgsmized_list])
         result_fgsm_random.append(model(x_fgsmized_random)["max_result"])
+    acc_various_models = []
+    for i in range(10):
+        result_from_single_model = [result_fgsm[i] for result_fgsm in result_fgsm_list]
+        acc_various_models.append(compute_accuracy(result_from_single_model,labels))
     acc = {"acc_origin" : compute_accuracy(result,labels),\
-    "acc_fgsm" : compute_accuracy(result_fgsm,labels),\
+    "acc_fgsm" : acc_various_models,\
     "acc_fgsm_random" : compute_accuracy(result_fgsm_random,labels)}
     print(acc)
 
---
+++ metrics.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_line_chart(acc,epsilon_0):
+    x = epsilon_0
+    y = acc
+    l=plt.plot(x,y)
+    plt.title('acc-epsilon0')
+    plt.xlabel('epsilon0')
+    plt.ylabel('acc')
+    plt.show()
+    plt.save("acc-epsilon")
+
+if __name__ == '__main__':
+    acc = [0.8181818181818182, 0.7402597402597403, 0.6493506493506493, 0.5909090909090909, \
+           0.5324675324675324, 0.512987012987013, 0.45454545454545453, 0.44805194805194803, \
+           0.42857142857142855, 0.42207792207792205]
+    epsilon_0 = [0.1*i for i in range(1,11)]
+    draw_line_chart(acc,epsilon_0)
